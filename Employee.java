bcea4179c0c53593a02edbc5136f21490e9a0177
Kia Rahmani <rahmank@purdue.edu>
2022-01-31 15:21:05 -0500

    Delete Employee.java

--- Employee.java
+++
@@ -1,9 +0,0 @@
-class Employee {
-       uuid emp_id;
-       boolean on_leave; 
-
-       public Employee(uuid id){
-               this.emp_id = id;
-               this.on_leave = false; 
-       }
-}
