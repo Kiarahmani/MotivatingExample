2e512286696632de27b0c1e693d9d76e13d07259
Kia Rahmani <rahmank@purdue.edu>
2022-01-31 15:15:14 -0500

    Create SampleDatabaseLibrary.py

---
+++ SampleDatabaseLibrary.py
@@ -0,0 +1,31 @@
+class SampleDatabaseLibrary:
+  def __init__(src, dst, rel_name, card):
+    self.src = src
+    self.dst = dst
+    self.rel_name = rel_name
+    self.card = card
+    # todo
+    
+  def add_operation(op):
+    # todo
+    return
+  
+  def add_assertion(ass):
+    # todo
+    return
+    
+  def select_one(tp, id):
+    # todo
+    return res
+  
+  def select_all(tp):
+    #todo
+    return res
+  
+  def select_associated(rel_name, obj):
+    # todo
+    return res
+  
+  def select_associated_inv(rel_name, obj):
+    # todo
+    return res
